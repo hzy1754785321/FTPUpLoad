3e5b3a02a111ece296dde5063c07c8064cb11357
hzy1754785321 <1754785321@qq.com>
2019-06-21 17:05:20 +0800

    test

---
+++ UpLoad/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace UpLoad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Uploadbutton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Uploadbutton
+            // 
+            this.Uploadbutton.Location = new System.Drawing.Point(219, 151);
+            this.Uploadbutton.Name = "Uploadbutton";
+            this.Uploadbutton.Size = new System.Drawing.Size(122, 35);
+            this.Uploadbutton.TabIndex = 0;
+            this.Uploadbutton.Text = "上传到FTP";
+            this.Uploadbutton.UseVisualStyleBackColor = true;
+            this.Uploadbutton.Click += new System.EventHandler(this.Uploadbutton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "选择要上传的文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(36, 74);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(305, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "上传";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 253);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Uploadbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Uploadbutton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ UpLoad/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace UpLoad
+{
+    public partial class Form1 : Form
+    {
+        private static string FTPCONSTR = "ftp://172.31.132.91:21";
+        private static string[] strpath;
+        private static string pathStr = "/data/";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Uploadbutton_Click(object sender, EventArgs e)
+        {
+            //    this.lbl_ftpStakt.Visible = true;   //设置上传信息标签可见
+            //     this.lbl_ftpStakt.Text = "连接服务器...";
+            var path = strpath;
+            try
+            {
+                for (int i = 0; i < path.Length; i++)
+                {
+                    var filename = path[i].ToString();
+                    CheckForIllegalCrossThreadCalls = false;
+                    //实例化事件类
+                    EntrustHandle fo = new EntrustHandle(filename);
+                    var myUpEventsHandler = new EntrustHandle.myUpEventsHandler(this.RunsOnWorkerThread);
+                    fo.startUpEvent += myUpEventsHandler;
+                    //      fo.startUpEvent += new EntrustHandle.myUpEventsHandler(this.RunsOnWorkerThread); //注册事件
+                    //      IAsyncResult result = fo.myUpEventsHandler.BeginInvoke("m", "n", null, null);
+                    fo.mythreadStart(); //调用类的方法
+                    FileInfo p = new FileInfo(path[i].ToString());
+                    //        uploadSQL(p.Name);  //上传到库
+                }
+                //label1.Text = "上传成功";
+            }
+            catch
+            {
+                string s = "";
+                for (int x = 0; x < path.Length; x++)
+                {
+                    FileInfo file = new FileInfo(path[x].ToString());
+                    s += file.Name + " ";
+                }
+                //   this.lbl_ftpStakt.Text = "上传失败";
+                MessageBox.Show(s.ToString() + " 上传失败", "提示");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //      label1.Text = "";
+            try
+            {
+                this.openFileDialog1.Multiselect = true;
+                this.openFileDialog1.ShowDialog();
+                var path = this.openFileDialog1.FileNames;  //获取openFileDialog控件选择的文件名数组
+                strpath = new string[path.Length];
+                for (int y = 0; y < path.Length; y++)
+                {
+                    strpath[y] = path[y];
+                }
+                //       textBox1.Text = strpath;
+            }
+            catch
+            {
+                //  this.lbl_ftpStakt.Text = "请选择文件!";
+                MessageBox.Show("请选择文件!");
+            }
+        }
+
+        //连接ftp上传
+        public void RunsOnWorkerThread(string _filename)
+        {
+            //IAsyncResult result =  BeginInvoke(, 2000, null, null);
+            ////阻塞线程
+            //mt.WaitOne();
+
+            //       Interlocked.Increment(ref flag);    //状态值+1
+
+            //   this.lbl_ftpStakt.Text = "连接服务器中...";
+            FileInfo fileInf = new FileInfo(_filename);
+            FtpWebRequest reqFTP;
+            // 根据uri创建FtpWebRequest对象
+            var filePath = FTPCONSTR + pathStr + fileInf.Name;
+             reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(filePath));
+            // ftp用户名和密码
+            //     reqFTP.Credentials = new NetworkCredential("record", "files");
+            // 默认为true，连接不会被关闭
+            // 在一个命令之后被执行
+
+            reqFTP.KeepAlive = false;
+            // 指定执行什么命令
+            reqFTP.Method = WebRequestMethods.Ftp.UploadFile;
+            // 指定数据传输类型
+            reqFTP.UseBinary = true;
+            // 上传文件时通知服务器文件的大小
+            reqFTP.ContentLength = fileInf.Length;
+            //long _length = fileInf.Length;  /////////
+            // 缓冲大小设置为2kb
+            int buffLength = 2048;  ////
+            byte[] buff = new byte[buffLength];
+            int contentLen;
+            // 打开一个文件流 (System.IO.FileStream) 去读上传的文件
+            FileStream fs = fileInf.OpenRead();
+
+            try
+            {
+                // 把上传的文件写入流
+                Stream strm = reqFTP.GetRequestStream();
+                // 每次读文件流的2kb
+                contentLen = fs.Read(buff, 0, buffLength);
+                int allbye = (int)fileInf.Length;
+                int startbye = 0;
+                this.progressBar1.Maximum = allbye;
+                this.progressBar1.Minimum = 0;
+                this.progressBar1.Visible = true;
+                //    this.lbl_ftpStakt.Visible = true;
+                //     this.lbl_ftpStakt.Text = "服务器连接中...";
+                // 流内容没有结束
+                while (contentLen != 0)
+                {
+                    // 把内容从file stream 写入 upload stream
+                    strm.Write(buff, 0, contentLen);
+                    contentLen = fs.Read(buff, 0, buffLength);
+                    startbye += contentLen;
+                    //                 this.lbl_ftpStakt.Text = "已上传:" + (int)(startbye / 1024) + "KB/" + "总长度:" + (int)(allbye / 1024) + "KB" + " " + " 文件名:" + fileInf.Name;
+                    progressBar1.Value = startbye;
+                }
+                // 关闭两个流
+                strm.Close();
+                fs.Close();
+                this.progressBar1.Visible = false;
+                MessageBox.Show("上传成功!");
+                //   this.lbl_ftpStakt.Text = "上传成功!";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "上传失败!");
+            }
+            //   Interlocked.Decrement(ref flag);
+            //   mt.ReleaseMutex();//释放线程
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string erroinfo = "";
+            var path = strpath[0];
+            FileInfo f = new FileInfo(path);
+            path = path.Replace("\\", "/");
+            path = FTPCONSTR + pathStr + f.Name;//这个路径是我要传到ftp目录下的这个目录下
+            FtpWebRequest reqFtp = (FtpWebRequest)FtpWebRequest.Create(new Uri(path));
+            reqFtp.UseBinary = true;
+            //       reqFtp.Credentials = new NetworkCredential(FTPUSERNAME, FTPPASSWORD);
+            reqFtp.KeepAlive = false;
+            reqFtp.Method = WebRequestMethods.Ftp.UploadFile;
+            reqFtp.ContentLength = f.Length;
+            int buffLength = 2048;
+            byte[] buff = new byte[buffLength];
+            int contentLen;
+            FileStream fs = f.OpenRead();
+            try
+            {
+                Stream strm = reqFtp.GetRequestStream();
+                contentLen = fs.Read(buff, 0, buffLength);
+                while (contentLen != 0)
+                {
+                    strm.Write(buff, 0, contentLen);
+                    contentLen = fs.Read(buff, 0, buffLength);
+                }
+                strm.Close();
+                fs.Close();
+                MessageBox.Show("成功");
+            }
+            catch (Exception ex)
+            {
+                erroinfo = string.Format("因{0},无法完成上传", ex.Message);
+                MessageBox.Show(erroinfo);
+            }
+        }
+    }
+}
